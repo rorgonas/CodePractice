b701782bff4a47c48deb3f29e51c486979138f2a
rorgonas <rorgonas@gmail.com>
2012-12-19 10:28:17 -0500

    Assignment: Create CartoonPerson class

---
+++ Assginments/problem_01/class/CartoonPerson.php
@@ -0,0 +1,31 @@
+<?php 
+
+
+/**
+ * @author Rolland Orgonas I.
+ *
+ *
+*/
+
+
+class CartoonPerson {
+	/**
+	 * @param min 4 properties that define the main characters + list of quotes
+	 */
+	public $husband;
+	public $wife;
+	public $oldest_child;
+	public $middle_child;
+	public $youngest_child;
+	public $quotes = array('Simpsons Roasting on an Open Fire', 'I\'m Spelling as Fast as I Can', 'Life on the Fast Lane');
+
+	
+}
+
+
+
+
+
+
+
+?>
