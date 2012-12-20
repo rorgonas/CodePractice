781a884e05b0471ffd041fd5ee7ed8da4a6d690d
rorgonas <rorgonas@gmail.com>
2012-12-20 08:38:20 -0500

    Remove extra code and implement some logic behind

--- Assginments/problem_01/class/CartoonPerson.php
+++ Assginments/problem_01/class/CartoonPerson.php
@@ -9,22 +9,50 @@
 
 
 class CartoonPerson {
-	/**
-	 * @param min 4 properties that define the main characters + list of quotes
-	 */
+	// Create 4 instance variables representing the main characters + list of quotes
+	public $person;
 	public $husband;
 	public $wife;
-	public $oldest_child;
-	public $middle_child;
-	public $youngest_child;
-	public $quotes = array('Simpsons Roasting on an Open Fire', 'I\'m Spelling as Fast as I Can', 'Life on the Fast Lane');
+	public $boy;
+	public $girl;
+	public $quotes = array('D\'oh!', 'Eat my shorts!', 'Life on the Fast Lane, ');
 
-	
+	public function __constructor($person) {
+		$this->person = $person;
+		$this->_constructor();  // call second constructor
+	}
+
+	// ToDo: Add one more constructor but how?
+	public function _constructor() {
+		// what to do here?
+	}
+
+	// Create toTalk method
+	public function toTalk($p) {
+		$random_quote_index = array_rand($this->quotes);
+		$message = $this->quotes[$random_quote_index];
+		echo $this->person.' says: '.$message.$p.'<br />';
+	}
+
 }
 
+class Main {
+	public function main() {
+		$homi = new CartoonPerson('Homer');
+		$lizi = new CartoonPerson('Lisa');
+		$barti = new CartoonPerson('Bart');
+		$margi = new CartoonPerson('Marge');
 
+		$homi->toTalk('Bart');
+	}
+}
 
+$homi = new CartoonPerson('Homer');
+$homi->toTalk('Bart');
+$homi->toTalk('Lisa');
 
+$lizi = new CartoonPerson('Lisa');
+$lizi->toTalk('Marge');
 
 
 
