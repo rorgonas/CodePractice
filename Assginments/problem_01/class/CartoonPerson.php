6aaa79abad9461ae21fbb9006228daf991bc5df7
rorgonas <rorgonas@gmail.com>
2012-12-20 09:10:46 -0500

    Need to fix the infinite loop

--- Assginments/problem_01/class/CartoonPerson.php
+++ Assginments/problem_01/class/CartoonPerson.php
@@ -9,27 +9,37 @@
 
 
 class CartoonPerson {
-	// Create 4 instance variables representing the main characters + list of quotes
+		// create cartoon person instance variable / property
 	public $person;
+		// Create 4 instance variables representing the main characters
 	public $husband;
 	public $wife;
 	public $boy;
 	public $girl;
+		// Create a list of quotes
 	public $quotes = array('D\'oh!', 'Eat my shorts!', 'Life on the Fast Lane!');
 
 	public function __construct($person) {
 		$this->person = $person;
-		$this->_construct();  // call second constructor
+		// $this->__construct($arg1, $arg2);  // Problem #3: call second constructor impossible
 	}
 
-	// ToDo: Add one more constructor
+	// Problem #3: Add one more constructor
 	// Cannot redeclare __construct() in the class
 
+	public function getRandomQuote() {
+		$random_key = array_rand($this->quotes);
+		$quote = $this->quotes[$random_key];
+		return $quote;
+	}
+
 	// Create toTalk method
-	public function toTalk($p) {
-		$random_quote_index = array_rand($this->quotes);
-		$message_to = $this->quotes[$random_quote_index];
-		echo $this->person.' says: '.$message_to.$p.'<br />';
+	public function toTalk($p) {		
+		// say something
+		echo $this->person.' says: '.$this->getRandomQuote().$p.'<br />';
+		// replay
+		$character = new CartoonPerson($p);
+		$character->toTalk($this->person);
 	}
 
 }
@@ -40,17 +50,15 @@ class Main {
 		$lizi = new CartoonPerson('Lisa');
 		$barti = new CartoonPerson('Bart');
 		$margi = new CartoonPerson('Marge');
-
-		$homi->toTalk('Bart');
 	}
 }
 
 $homi = new CartoonPerson('Homer');
 $homi->toTalk('Bart');
-$homi->toTalk('Lisa');
+// $homi->toTalk('Lisa');
 
-$lizi = new CartoonPerson('Lisa');
-$lizi->toTalk('Marge');
+// $lizi = new CartoonPerson('Lisa');
+// $lizi->toTalk('Marge');
 
 
 
